a466a46a7ffcc538025b86b867c97ea59ec69b2c
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-10-24 03:27:09 +0600

    Error Page added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,9 @@ const Error = () => {
     return (
         <div className='max-w-7xl mx-auto'>
             <Navbar></Navbar>
-           
+            <div className='flex justify-center items-center my-20'>
+                <img src={error} alt="error" />
+            </div>
         </div>
     );
 };
